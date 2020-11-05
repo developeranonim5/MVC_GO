324ee1eabd95abaa45f86970189c471bcf52f2a9
amriyahya <yahya.amryk@gmail.com>
2020-11-05 20:29:12 +0700

    MVC GO

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"/app/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*")
+
+	router.POST("/api/v1/antrian", controller.AddAntrianHandler)
+	router.GET("/api/v1/antrian/status", controller.GetAntrianHandler)
+	router.PUT("/api/v1/antrian/id/:idAntrian", controller.UpdateAntrianHandler)
+	router.DELETE("/api/v1/antrian/id/:idAntrian/delete", controller.DeleteAntrianHandler)
+	router.GET("/antrian", controller.PageAntrianHandler)
+	router.Run(":8080")
+}
